d12b8b7e4331d52f2632cf62d07ac92dad7394cb
Vojticek Florian (IT OS WP ADO OD) <Florian.Vojticek@infineon.com>
2024-07-19 12:29:07 +0200

    Add Client and Handler

--- app/src/main/java/com/example/frontend/MainActivity.kt
+++ app/src/main/java/com/example/frontend/MainActivity.kt
@@ -5,17 +5,41 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.frontend.database.Database_Helper
+import com.example.frontend.models.HobbyModel
 
 class MainActivity : AppCompatActivity() {
+
+    /*
+    var dbHandler : Database_Helper?= null
+    var hobby : HobbyModel? = null
+    */
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-            //test
+
+        /*
+        dbHandler = Database_Helper(this)
+
+        hobby = HobbyModel().apply {
+            id = 1
+            user = "John Doe"
+            title = "Hiking"
+            description = "Exploring new trails"
+            number = 5
+            date = "2023-03-15"
+            location = "Mount Everest"
+            latitude = 27.9881
+            longitude = 86.9250
         }
+
+        addHobby(hobby!!)
+        */
     }
+    /*
+    private fun addHobby(hobby: HobbyModel){
+        dbHandler!!.addHobby(hobby)
+    }
+    */
 }
