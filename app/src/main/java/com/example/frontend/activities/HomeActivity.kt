e17e1444b6b72928e976f91515c686907092cf80
danieltabatabai <danieltabse@gmail.com>
2024-07-22 13:41:14 +0200

    Login merged into dev

--- app/src/main/java/com/example/frontend/activities/HomeActivity.kt
+++ app/src/main/java/com/example/frontend/activities/HomeActivity.kt
@@ -1,7 +1,8 @@
-package com.example.frontend
+package com.example.frontend.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.frontend.R
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/frontend/activities/LoginActivity.kt
+++ app/src/main/java/com/example/frontend/activities/LoginActivity.kt
@@ -1,4 +1,4 @@
-package com.example.frontend
+package com.example.frontend.activities
 
 import android.content.Intent
 import android.os.Bundle
@@ -6,6 +6,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.frontend.R
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
@@ -82,12 +83,12 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
-    // Nach erfolgreicher Anmeldung zur HomeActivity navigieren
+    // Nach erfolgreicher Anmeldung zu HomeActivity navigieren
     private fun handleSignInResult(account: GoogleSignInAccount) {
         navigateToHome()
     }
 
-    // Funktion, um zur HomeActivity zu navigieren
+    // Funktion, um zu HomeActivity zu navigieren
     private fun navigateToHome() {
         val intent = Intent(this, HomeActivity::class.java)
         startActivity(intent)
